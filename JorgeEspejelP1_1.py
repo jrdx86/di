e957ac727ccc63597e935e96ea1a8ac3c98a8ed2
jrdx86 <jrdx86@gmail.com>
2018-10-16 12:26:28 +0200

    añadido flitrado .isdigit()

--- JorgeEspejelP1_1.py
+++ JorgeEspejelP1_1.py
@@ -1,16 +1,14 @@
-#! /usr/bin/python3
-
 indice = input("Dime cuantas palabras tiene la lista: ")
-index = int(indice)a
-lista = []
 
-if index > 0:
-	for i in range(index):
-		nombre=input("Digame la palabra "+ str(i+1) +": ")
-		lista.append(nombre)
-	
+if indice.isdigit():
+	index = int (indice)
+	if index > 0:
+		lista = []
 
-	print(lista)
+		for i in range(index):
+			nombre=input("Digame la palabra "+ str(i+1) +": ")
+			lista.append(nombre)
+		print(lista)
+
 else:
-	print("¡Imposible!")
-
+	print("Imposible")
