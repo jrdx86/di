878a1a3911ff5bb2e0478cff31a21e47bcdc3e5f
Jordi <jrdx86>
2018-10-21 23:17:42 +0200

    rectificado

--- P8/JorgeEspejelP2_3.py
+++ P8/JorgeEspejelP2_3.py
@@ -11,7 +11,7 @@ if introduction.isnumeric():
  		list = []
  		i=1
  		
- 		for i in range(1,index):
+ 		for i in range(2,index):
  			if (i%2!=0 or i==2) and (i%3!=0 or i==3) and (i%5!=0 or i==5) and (i%7!=0 or i==7):
  				list.append(i)
  				
