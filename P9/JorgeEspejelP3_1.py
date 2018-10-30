8c12b3d19e9b10d1ea01a5fe05d07474336929a7
jrdx86 <jrdx86@gmail.com>
2018-10-30 21:32:38 +0100

    P9

---
+++ P9/JorgeEspejelP3_1.py
@@ -0,0 +1,11 @@
+ #! /usr/bin/python3
+
+def area_triangulo(b,h):
+
+    area=b*h/2
+    return print('El area del triángulo es: ', area)
+
+b=int(input('Ingrese la base: '))
+h=int(input('Ingrese la altura: '))
+area_triangulo(b,h)
+
