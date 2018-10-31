ef2ee049b7d55a53b24c88c4678061192902d008
jrdx86 <jrdx86@gmail.com>
2018-10-31 11:49:43 +0100

    añadida P3_2

--- P9/JorgeEspejelP3_2.py
+++ P9/JorgeEspejelP3_2.py
@@ -1,5 +1,19 @@
 
-def distancia(*tramo):
-	total=0
-	for distancia in tramos:
-		total+= tramos
+def distancia(tramo):
+    laSuma = 0
+    for i in range(0,len(tramo)):
+        laSuma +=  tramo[i]
+    return print("La suma de tramos es: ",laSuma)
+
+lista = []
+
+print("Introduce tramos y cuando quieras parar introduce una letra cualquiera")
+while(True):
+	try:
+		tramo=int(input("Introduce un tramo: "))
+		lista.append(tramo)
+	except:
+		break
+
+
+distancia(lista)
