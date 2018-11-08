5f655d0514c4051b0e166a9bdbcf3dc1247ea043
jrdx86 <jrdx86@gmail.com>
2018-11-08 09:57:41 +0100

    universidad sin comentarios

--- p10/p10_universidad.py
+++ p10/p10_universidad.py
@@ -4,8 +4,11 @@ class miembro():
 		self.nombre = nombre 
 		self.direccion = direccion
 		self.dni=dni
+
 	def __str__(self):
 		return str(self.nombre)+","+str(self.direccion)+","+str(self.dni)
+
+
 
 
 
@@ -16,7 +19,9 @@ class profesor(miembro):
 
 	def __str__(self):
 		return str(self.nombre)+","+str(self.direccion)+","+str(self.dni)+","+str(self.n_reg)
-	
+
+
+
 
 class estudiante(miembro):
 	def __init__(self, nombre, direccion,dni,num_estudiante):
@@ -26,6 +31,33 @@ class estudiante(miembro):
 	def __str__(self):
 		return str(self.nombre)+","+str(self.direccion)+","+str(self.dni)+","+str(self.num_estudiante)
 
+
+
 class asignatura():
-	nombre=""
-	codAsignatura=""
+	def __init__(self,asi,codigo):
+		self.asi=asi
+		self.codigo=codigo
+		self.profesores=[]
+
+	def añade_profesores(self,profesor):
+		self.profesores.append(profesor)
+	
+	def mostrar_profesores(self):
+		print("Los profesores son: ")
+		for profesor in self.profesores:
+			print("*Nombre: ",profesor.nombre," DNI: ",profesor.dni, " Edad: ",profesor.direccion," n_reg: ",profesor.n_reg)
+				
+	def __str__(self):
+		return "Nombre: "+str(self.asi)+" CódAsignat.:"+str(self.codigo)
+		
+			
+
+
+
+p1=profesor("Luis", "calle lepanto",4488995544j,34)
+p2=profesor("Juan", "calle lepanto",4488994564j,65)
+matematicas=asignatura("matematicas",56)
+print(matematicas)
+matematicas.añade_profesores(p1)
+matematicas.añade_profesores(p2)
+matematicas.mostrar_profesores()
